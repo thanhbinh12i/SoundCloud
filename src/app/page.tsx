3bec627b8559af0decf0ae21b59b6dccdd1cdd2a
thanhbinh12ii <nptbinh17092004@gmail.com>
2024-11-21 21:03:31 +0700

    layout:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
+import AppFooter from "@/components/footer/app.footer";
+import AppHeader from "@/components/header/app.header";
+import MainSlider from "@/components/main/main.slider";
 
 export default function HomePage() {
   return (
-    <>Hello world</>
+    <>
+      <AppHeader />
+      <MainSlider />
+      <MainSlider />
+      <AppFooter />
+    </>
   )
 }
---
+++ src/components/header/app.header.tsx
@@ -0,0 +1,178 @@
+'use client'
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import MoreIcon from '@mui/icons-material/MoreVert';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+
+
+//styled-component
+const Search = styled('div')(({ theme }) => ({
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: alpha(theme.palette.common.white, 0.15),
+      '&:hover': {
+            backgroundColor: alpha(theme.palette.common.white, 0.25),
+      },
+      marginRight: theme.spacing(2),
+      marginLeft: 0,
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(3),
+            width: 'auto',
+      },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+      padding: theme.spacing(0, 2),
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+      color: 'inherit',
+      '& .MuiInputBase-input': {
+            padding: theme.spacing(1, 1, 1, 0),
+            paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+            transition: theme.transitions.create('width'),
+            width: '100%',
+            [theme.breakpoints.up('md')]: {
+                  width: '400px',
+            },
+      },
+}));
+
+export default function AppHeader() {
+      const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+      const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
+            React.useState<null | HTMLElement>(null);
+
+      const isMenuOpen = Boolean(anchorEl);
+      const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+      const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+            setAnchorEl(event.currentTarget);
+      };
+
+      const handleMobileMenuClose = () => {
+            setMobileMoreAnchorEl(null);
+      };
+
+      const handleMenuClose = () => {
+            setAnchorEl(null);
+            handleMobileMenuClose();
+      };
+
+      const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+            setMobileMoreAnchorEl(event.currentTarget);
+      };
+
+      const menuId = 'primary-search-account-menu';
+      const renderMenu = (
+            <Menu
+                  anchorEl={anchorEl}
+                  id={menuId}
+                  keepMounted
+                  open={isMenuOpen}
+                  onClose={handleMenuClose}
+                  transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                  anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+            >
+                  <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+                  <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+            </Menu>
+      );
+
+      const mobileMenuId = 'primary-search-account-menu-mobile';
+      const renderMobileMenu = (
+            <Menu
+                  anchorEl={mobileMoreAnchorEl}
+                  anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                  }}
+                  id={mobileMenuId}
+                  keepMounted
+                  transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                  }}
+                  open={isMobileMenuOpen}
+                  onClose={handleMobileMenuClose}
+            >
+                  <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+                  <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+            </Menu>
+      );
+
+      return (
+            <Box sx={{ flexGrow: 1 }}>
+                  <AppBar
+                        position="static"
+                        sx={{ background: '#333' }}
+                  >
+                        <Container>
+                              <Toolbar>
+                                    <img src='assest/logo.png' style={{ background: 'linear-gradient(#f70,#f30)', padding: '10px', marginRight: '10px' }} />
+                                    <Typography
+                                          variant="h6"
+                                          noWrap
+                                          component="div"
+                                          sx={{ display: { xs: 'none', sm: 'block' } }}
+                                    >
+                                          Binhdaynee
+                                    </Typography>
+                                    <Search>
+                                          <SearchIconWrapper>
+                                                <SearchIcon />
+                                          </SearchIconWrapper>
+                                          <StyledInputBase
+                                                placeholder="Search…"
+                                                inputProps={{ 'aria-label': 'search' }}
+                                          />
+                                    </Search>
+                                    <Box sx={{ flexGrow: 1 }} />
+                                    <Box sx={{
+                                          display: { xs: 'none', md: 'flex' },
+                                          gap: "20px",
+                                          alignItems: "center",
+                                          cursor: "pointer"
+                                    }}>
+                                          <span>Playlist</span>
+                                          <span>Likes</span>
+                                          <span>Upload</span>
+                                          <Avatar alt="Remy Sharp" src='assest/avatar.jpg' onClick={handleProfileMenuOpen} />
+                                    </Box>
+                                    <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+                                          <IconButton
+                                                size="large"
+                                                aria-label="show more"
+                                                aria-controls={mobileMenuId}
+                                                aria-haspopup="true"
+                                                onClick={handleMobileMenuOpen}
+                                                color="inherit"
+                                          >
+                                                <MoreIcon />
+                                          </IconButton>
+                                    </Box>
+                              </Toolbar>
+                        </Container>
+                  </AppBar>
+                  {renderMobileMenu}
+                  {renderMenu}
+            </Box>
+      );
+}
